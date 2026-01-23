647c028ea5882f4b45fccf79b4f255e099218f11
kanta1207 <kan0712livepool@gmail.com>
2026-01-22 17:15:34 -0800

    feat: draw screen green

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@
 #![no_main] // Do not use Rust’s default `main()`. We define our own entry point that UEFI calls.
 
 use core::arch::asm;
+use core::cmp::min;
 use core::mem::offset_of; // Used to verify struct field offsets (to ensure they match the C layout).
 use core::mem::size_of; // Used for size calculations (e.g., converting framebuffer size to element count).
 use core::panic::PanicInfo;
 use core::ptr::null_mut; // Used to create null pointers for UEFI calls.
-use core::slice; // Used to create slices from raw pointers (treat framebuffer as an array).
 
 // Type aliases to minimally represent UEFI types on the Rust side.
 // UEFI is a C-based world, so concepts like `void*` and `handle` appear.
@@ -25,31 +25,14 @@ enum EfiStatus {
 // --- Entry point called by UEFI ---
 #[no_mangle] // Disable Rust name mangling so UEFI can find the symbol.
 fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
-    // Purpose: Locate and obtain the UEFI Graphics Output Protocol (GOP).
-    // Once GOP is available, we can get the framebuffer (raw pixel memory) address.
-    let efi_graphics_output_protocol = locate_graphic_protocol(efi_system_table).unwrap();
+    let mut vram = init_vram(efi_system_table).expect("init_vram failed");
 
-    // Purpose: Obtain the framebuffer (VRAM-equivalent) base address and size.
-    let vram_addr = efi_graphics_output_protocol.mode.frame_buffer_base;
-    let vram_byte_size = efi_graphics_output_protocol.mode.frame_buffer_size;
-
-    // Purpose: Treat the raw address (usize) as a `u32` array.
-    //
-    // - In most environments, the GOP framebuffer is 32bpp (1 pixel = 4 bytes),
-    //   so we can safely write pixels as `u32`.
-    // - This is unsafe because Rust cannot guarantee that this address is valid
-    //   or writable for the given length.
-    let vram = unsafe {
-        slice::from_raw_parts_mut(
-            vram_addr as *mut u32,             // Base address
-            vram_byte_size / size_of::<u32>(), // Number of elements (bytes → u32 count)
-        )
-    };
-
-    // Purpose: Fill all pixels with white (0xFFFFFFFF) to confirm that drawing works.
-    // This is a preliminary step before implementing a println-like text output.
-    for e in vram {
-        *e = 0xFFFFFFFF;
+    for y in 0..vram.height{
+        for x in 0..vram.width{
+            if let Some(pixel) = vram.pixel_at_mut(x, y){
+                *pixel = 0x00ff00;
+            }
+        }
     }
 
     // Eventually we want to draw text here, but we do not have println yet.
@@ -58,7 +41,6 @@ fn efi_main(_image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
         hlt();
     }
 }
-
 // --- UEFI table definitions (only the required parts) ---
 // Purpose: Define the UEFI System Table and Boot Services Table in Rust
 // using a C-compatible layout so we can call into UEFI.
@@ -216,4 +198,73 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {
         hlt();
     }
+}
+
+trait Bitmap {
+    fn bytes_per_pixel(&self) -> i64;
+    fn pixels_per_line(&self) -> i64;
+    fn width(&self) -> i64;
+    fn height(&self) -> i64;
+    fn buf_mut(&mut self) -> *mut u8;
+    /// # Safety
+    ///
+    /// Returned pointer is valid as long as the given coordinates are valid
+    /// which means that passing is_in_*_range tests.
+    unsafe fn unchecked_pixel_at_mut(&mut self, x: i64, y: i64) -> *mut u32 {
+        self.buf_mut().add(
+            ((y * self.pixels_per_line() + x) * self.bytes_per_pixel())
+                as usize,
+        ) as *mut u32
+    }
+    fn pixel_at_mut(&mut self, x: i64, y: i64) -> Option<&mut u32> {
+        if self.is_in_x_range(x) && self.is_in_y_range(y) {
+            // SAFETY: (x, y) is always validated by the checks above.
+            unsafe { Some(&mut *(self.unchecked_pixel_at_mut(x, y))) }
+        } else {
+            None
+        }
+    }
+    fn is_in_x_range(&self, px: i64) -> bool {
+        0 <= px && px < min(self.width(), self.pixels_per_line())
+    }
+    fn is_in_y_range(&self, py: i64) -> bool {
+        0 <= py && py < self.height()
+    }
+}
+
+
+#[derive(Clone, Copy)]
+struct VramBufferInfo {
+    buf: *mut u8,
+    width: i64,
+    height: i64,
+    pixels_per_line: i64,
+}
+
+impl Bitmap for VramBufferInfo {
+    fn bytes_per_pixel(&self) -> i64 {
+        4
+    }
+    fn pixels_per_line(&self) -> i64 {
+        self.pixels_per_line
+    }
+    fn width(&self) -> i64 {
+        self.width
+    }
+    fn height(&self) -> i64 {
+        self.height
+    }
+    fn buf_mut(&mut self) -> *mut u8 {
+        self.buf
+    }
+}
+
+fn init_vram(efi_system_table: &EfiSystemTable) -> Result<VramBufferInfo> {
+    let gp = locate_graphic_protocol(efi_system_table)?;
+    Ok(VramBufferInfo {
+        buf: gp.mode.frame_buffer_base as *mut u8,
+        width: gp.mode.info.horizontal_resolution as i64,
+        height: gp.mode.info.vertical_resolution as i64,
+        pixels_per_line: gp.mode.info.pixels_per_scan_line as i64,
+    })
 }
